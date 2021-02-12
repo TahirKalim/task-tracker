736d37d438f9baf34d951d5fa4dd2f35ef4ad4bc
tahir kalim <thrkalim@gmail.com>
2021-02-12 05:52:04 +0100

    header and task

---
+++ src/components/Tasks.js
@@ -0,0 +1,37 @@
+// import {useState} from 'react'
+import Task from './Task'
+
+const Tasks = ({tasks, onDelete}) => {
+
+    return (
+        <>
+            {tasks.map((task) => (
+               <Task key={task.id} task={task} onDelete={onDelete} />
+           ))}
+        </>
+         
+    )
+}
+
+export default Tasks
+
+// const [tasks, setTasks] = useState([
+//     {
+//         id: 1,
+//         text: 'Doctors Appointment',
+//         day: 'Feb 5th at 2:30pm',
+//         reminder: true,
+//     },
+//     {
+//         id: 2,
+//         text: 'Metting at School',
+//         day: 'Feb 6th at 1:30pm',
+//         reminder: true,
+//     },
+//     {
+//         id: 3,
+//         text: 'Food Shopping',
+//         day: 'Feb 5th at 2:30pm',
+//         reminder: false,
+//     },
+// ])
